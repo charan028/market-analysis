91ab539c7661c7ba3214bbba7a9517dec61ad850
charan028 <67135079+charan028@users.noreply.github.com>
2021-11-25 20:03:34 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a Store Sales package",
+    author="harish2f",
+    packages=find_packages(),
+    license="MIT"
+)
